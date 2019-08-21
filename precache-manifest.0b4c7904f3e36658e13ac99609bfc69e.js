3ba7273a3c0f2b691c130f10727aa2e49cd41be3
TaiNgo6798 <taingo6798@gmail.com>
2019-08-21 13:44:01 +0700

    Updates

---
+++ precache-manifest.0b4c7904f3e36658e13ac99609bfc69e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecfd29fa8fcd563f5e944f43a10d915",
+    "url": "/myBlog_React/index.html"
+  },
+  {
+    "revision": "1a6f4a16e1995e43968f",
+    "url": "/myBlog_React/static/css/main.3a840c32.chunk.css"
+  },
+  {
+    "revision": "ec90188f084b4e489897",
+    "url": "/myBlog_React/static/js/2.d0cca4e3.chunk.js"
+  },
+  {
+    "revision": "1a6f4a16e1995e43968f",
+    "url": "/myBlog_React/static/js/main.c7918b29.chunk.js"
+  },
+  {
+    "revision": "e4e205154e425f025bbd",
+    "url": "/myBlog_React/static/js/runtime~main.75ba92da.js"
+  }
+]);
